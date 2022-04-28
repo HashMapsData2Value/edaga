2c0461de86613a1aaa0e2b2dea53923bbfb31256
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2022-04-29 00:56:32 +0200

    removes unused stuff for nwo

--- src/Replies.js
+++ src/Replies.js
@@ -6,18 +6,6 @@ function Replies() {
   let { txId } = useParams();
   console.log(txId);
 
-  useEffect(() => {
-    getDownvotes();
-  }, [])
-
-  const getDownvotes = async () => {
-    const prefix = btoa(`ARC00-0;d;${txId}`);
-    const url = `https://mainnet-idx.algonode.cloud/v2/accounts/K22E7O64EMVMBVPUQ53VVXN2U4WCYL7XN6PHOYMNNEBSNM6RMMKJZ3OAMI/transactions?note-prefix=${prefix}`
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
-  }
-
   return (
   <div>
     <Text>Replies {txId}</Text>
