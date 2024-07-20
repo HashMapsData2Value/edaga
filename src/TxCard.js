10a89f4b55173e649ca1fef3b324461c33bfad1c
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2024-07-21 00:55:22 +0200

    Update TxCard.js

--- src/TxCard.js
+++ src/TxCard.js
@@ -11,9 +11,9 @@ const TxCard = ({ id, sender, fee, confirmedRound, body }) => {
       <CardBody pad="medium"><Box>{body}</Box></CardBody>
       <CardFooter pad={{ horizontal: "small" }} background="light-2">
         <Text>Fee: {fee}</Text>
-        <Text>Id: <a href={'https://testnet.algoexplorer.io/tx/' + id}>{getShortenedBase32(id)}</a></Text>
+        <Text>Id: <a href={'https://testnet.explorer.perawallet.app/tx/' + id}>{getShortenedBase32(id)}</a></Text>
         <Text><Link to={`/replies/${id}`}>See Replies</Link></Text>
-        <Text>Block: <a href={'https://testnet.algoexplorer.io/block/' + confirmedRound}>{confirmedRound}</a></Text>
+        <Text>Block: <a href={'https://testnet.explorer.perawallet.app/block/' + confirmedRound}>{confirmedRound}</a></Text>
       </CardFooter>
     </Card>
   )
